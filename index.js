5d66bec885f66cd9a695988fe7320e8814265a26
brandonabajelo-zoom <brandon.abajelo@zoom.us>
2021-05-12 23:16:54 -0700

    test

--- index.js
+++ index.js
@@ -21,13 +21,13 @@ const app = express()
 // Add Middlewares
 app.use([cors(), express.json(), express.urlencoded({ extended: false })]);
 app.options('*', cors());
-app.use((req, res, next) => {
-  const ALLOWED_IPS = ['38.99.100', '38.99.114', '144.178.83', '63.233.134', '135.26.244'];
-  if (isProduction && !ALLOWED_IPS.includes(req.ip)) {
-    return res.status(500).json({ message: "Connect to the VPN" });
-  }
-  next();
-});
+// app.use((req, res, next) => {
+//   const ALLOWED_IPS = ['38.99.100', '38.99.114', '144.178.83', '63.233.134', '135.26.244'];
+//   if (isProduction && !ALLOWED_IPS.includes(req.ip)) {
+//     return res.status(500).json({ message: "Connect to the VPN" });
+//   }
+//   next();
+// });
 
 /**
  * For the purposes of this demo application, creating/updating resources will
